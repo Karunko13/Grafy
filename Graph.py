7e2d5952e7a8af5af2ef108d9b37c91f9fa4beb6
mirek0707 <80774381+mirek0707@users.noreply.github.com>
2022-04-24 19:15:44 +0200

    finished drawing and fixed Graphs

--- Graph.py
+++ Graph.py
@@ -2,38 +2,35 @@ from turtle import position
 from conversions import *
 import tkinter as tk
 import math
+
 class Graph:
-    adjacencyMatrix = None #macierz sasiedztwa
-    adjacencyList = None #lista sasiedztwa
-    incidenceMatrix = None #macierz incydencji
+    adjacencyMatrix = None  # macierz sasiedztwa
+    adjacencyList = None  # lista sasiedztwa
+    incidenceMatrix = None  # macierz incydencji
     temp = None
-    
-    def __init__(self, file_path=None, graph_representation="a_m", info=True):
+
+    def __init__(self, file_path=None, graph_representation="a_m"):
         if file_path is None:
-            self.data = [[None]]
-            self.represent = "a_m"
+            data = [[None]]
         else:
             with open(file_path, 'r') as f:
                 data = [[int(val) if '0' not in val else 0 for val in line.split(' ')] if line != '\n' else [] for
                         line in f]
-            self.data = data
-            self.represent = graph_representation
 
-        if info is True:
-                print("Graph represented by " +
-                      ("adjacency matrix." if self.represent == "a_m"
-                       else ("incidence matrix." if self.represent == "i_m"
-                             else "adjacency list.")))
-        if self.represent == "a_m":
-            self.adjacencyMatrix = self.data
+        print("Graph represented by " + ("adjacency matrix." if graph_representation == "a_m"
+                                         else ("incidence matrix." if graph_representation == "i_m" else "adjacency list.")))
+        if graph_representation == "a_m":
+            self.adjacencyMatrix = data
             self.adjacencyList = adj_matrix_to_adj_list(self.adjacencyMatrix)
-            self.incidenceMatrix = adj_matrix_to_inc_matrix(self.adjacencyMatrix)
-        elif self.represent == "i_m":
-            self.incidenceMatrix = self.data
-            self.adjacencyMatrix = inc_matrix_to_adj_matrix(self.incidenceMatrix)
+            self.incidenceMatrix = adj_matrix_to_inc_matrix(
+                self.adjacencyMatrix)
+        elif graph_representation == "i_m":
+            self.incidenceMatrix = data
+            self.adjacencyMatrix = inc_matrix_to_adj_matrix(
+                self.incidenceMatrix)
             self.adjacencyList = inc_matrix_to_adj_list(self.incidenceMatrix)
         else:
-            self.adjacencyList = self.data
+            self.adjacencyList = data
             self.adjacencyMatrix = adj_list_to_adj_matrix(self.adjacencyList)
             self.incidenceMatrix = adj_list_to_inc_matrix(self.adjacencyList)
 
@@ -56,34 +53,39 @@ class Graph:
         center_x = width / 2
         center_y = height / 2
         R = center_x * 3/4
-        r = R / n * (1 if n > 2 else 0.5)
+        if n > 3:
+            r = R / n
+        else:
+            r = R / n * 0.5
 
         window = tk.Tk()
         window.geometry("800x800")
         canvas = tk.Canvas(window, height=height, width=width, bg="white")
 
-        positions=[]
-
+        positions = []
+        # set positions of verticles
         for i in range(n):
-            v_angle = i * alfa
             positions.append([0.0, 0.0])
             if n > 1:
-                positions[i][0] = center_y + R * math.sin(v_angle)
-                positions[i][1] = center_x - R * math.cos(v_angle)
-
+                positions[i][0] = center_y + R * math.sin(i * alfa)
+                positions[i][1] = center_x - R * math.cos(i * alfa)
+            else:
+                positions[i][0] = center_x
+                positions[i][1] = center_y
+        #draw edges
         for i in range(1, n):
             for j in range(0, i):
                 if self.adjacencyMatrix[i][j] == 1:
-                    canvas.create_line(positions[i][0], positions[i][1], positions[j][0], positions[j][1], fill="black")
-
+                    canvas.create_line(
+                        positions[i][0], positions[i][1], positions[j][0], positions[j][1], fill="black", width=2)
+        #draw verticles and numbers
         for i in range(n):
-            v_angle = i * alfa
             canvas.create_oval(positions[i][0]-r, positions[i][1]-r,
                                positions[i][0]+r, positions[i][1]+r,
-                               fill="green", outline="black", width=3)
-            canvas.create_text(positions[i][0] + (1 + n/7) * r * math.sin(v_angle),
-                               positions[i][1] - (1 + n/7) * r * math.cos(v_angle),
-                               text=i+1, font=("Verdana", max(int(20 - 2*n/10), 10)))
+                               fill="lime", outline="black", width=3)
+            canvas.create_text(positions[i][0],
+                               positions[i][1],
+                               text=i+1, font=("Comic Sans", int(3*r/4), "bold"), anchor=tk.CENTER)
 
         canvas.pack()
         window.mainloop()
--- __main__.py
+++ __main__.py
@@ -1,10 +1,8 @@
-import sys
 from conversions import *
 from Graph import Graph
 
 if __name__ == '__main__':
-    g1 = Graph("z1_am.txt", "a_m", True)
-    #g1.print_all_representations()
-    #print(g1.adjacencyList)
+    g1 = Graph("z1_am.txt", "a_m")
+    g1.print_all_representations()
     g1.draw()
 
