72d3e0ef558ff721ca39cf1f130520e80809559a
Mateusz <mateusz.nizio13@gmail.com>
2022-04-24 13:13:32 +0200

    Basic draw function

--- Graph.py
+++ Graph.py
@@ -1,11 +1,12 @@
 from conversions import *
-
-
+import tkinter as tk
+import math
 class Graph:
-    adjacencyMatrix = None
-    adjacencyList = None
-    incidenceMatrix = None
-
+    adjacencyMatrix = None #macierz sasiedztwa
+    adjacencyList = None #lista sasiedztwa
+    incidenceMatrix = None #macierz incydencji
+    temp = None
+    
     def __init__(self, file_path=None, graph_representation="a_m", info=True):
         if file_path is None:
             self.data = [[None]]
@@ -42,3 +43,48 @@ class Graph:
         print(self.adjacencyList)
         print(self.adjacencyMatrix)
         print(self.incidenceMatrix)
+
+    def draw(self, img_width=600, img_height=600):
+        
+        print("here")
+        print(self.adjacencyMatrix)
+        self.temp = self.adjacencyMatrix
+
+        if self.temp is None:
+            print("Graph is empty (no temp) - cannot draw the graph.")
+            return
+
+        n = len(self.temp)
+        angle = 2 * math.pi / n
+
+        g_center_width = img_width / 2
+        g_center_height = img_height / 2
+        g_r = g_center_width * 2 / 3
+        v_r = g_r / n * (1 if n > 2 else 0.5)
+
+        root = tk.Tk()
+        root.geometry(str(img_height)+"x"+str(img_width))
+        canvas = tk.Canvas(root, height=img_height, width=img_width, bg="white")
+
+        positions = [[0.0]*2 for _ in range(n)]
+
+        for i in range(n):
+            v_angle = i * angle
+            positions[i][0] = g_center_height + (g_r * math.sin(v_angle) if n > 1 else 0)
+            positions[i][1] = g_center_width - (g_r * math.cos(v_angle) if n > 1 else 0)
+
+            fill_color = "black"
+
+            canvas.create_oval(positions[i][0]-v_r, positions[i][1]-v_r,
+                               positions[i][0]+v_r, positions[i][1]+v_r,
+                               fill=fill_color)
+            canvas.create_text(positions[i][0] + (1 + n/7) * v_r * math.sin(v_angle),
+                               positions[i][1] - (1 + n/7) * v_r * math.cos(v_angle),
+                               text=i+1, font=("Verdana", max(int(20 - 2*n/10), 10)))
+        for i in range(1, n):
+            for j in range(0, i):
+                if self.temp[i][j] is 1:
+                    canvas.create_line(positions[i][0], positions[i][1], positions[j][0], positions[j][1], fill="black")
+
+        canvas.pack()
+        root.mainloop()
--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from Graph import Graph
 
 if __name__ == '__main__':
     g1 = Graph("z1_am.txt", "a_m", True)
-    g1.print_all_representations()
-
+    #g1.print_all_representations()
+    #print(g1.adjacencyList)
+    g1.draw()
 
