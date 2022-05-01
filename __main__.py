2e3f343975bfd3e052e79c94a3245dd91d281055
Mateusz <mateusz.nizio13@gmail.com>
2022-05-01 16:47:47 +0200

    changes in main

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
+import sys
 from conversions import *
 from Graph import Graph
+from generators import *
 
 if __name__ == '__main__':
-    g1 = Graph("z1_am.txt", "a_m")
-    g1.print_all_representations()
+    # g1 = Graph("z1_am.txt", "a_m", True)
+    # g1.print_all_representations()
+    # g1.draw()
+    print(generate_n_l(12, 12, 'test2.txt'))
+    #print(generate_n_p(9, 0.5, 'test.txt'))
+    g1 = Graph("test2.txt", "a_m")
     g1.draw()
 
