a412859a8daaa95ff8574cc643cd0ac64141525a
baloniq89 <83978467+baloniq89@users.noreply.github.com>
2022-05-03 18:11:37 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,21 +1,32 @@
+from utils.conversions import *
+from utils.degree_sequence import *
+from utils.generators import *
 from utils.regular_graph_randomization import *
 from Graph import Graph
 
+
 if __name__ == '__main__':
-    # g1 = Graph("z1_am.txt", "a_m")
+    # g1 = Graph("txt_files/z1_am.txt", "a_m")
     # g1.print_all_representations()
     # g1.draw()
+    # g1.components()
+    #print_graphical([4,2,2,3,2,1,4,2,2,2,2])
 
-    # print_graphical([4,2,2,3,2,1,4,2,2,2,2])
-    #zad2
-    #g2 = Graph(randomize_graph([4, 4, 3, 1, 2], 10), "a_l", False) #not graphical sequence
-    g2 = Graph(randomize_graph([4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2], 10), "a_l", False)
+
+
+    # g2 = graphical_to_graph([4,2,2,3,2,1,4,2,2,2,2])
+    # g2.draw()
+    # g2.check_hamilton()
+
+    # print(generate_n_l(12,5, 'txt_files/test.txt'))
+    # g3=Graph('txt_files/test.txt', "a_m")
+    # g3.draw()
+
+    g2 = Graph(randomize_graph([4,2,2,3,2,1,4,2,2,2,2], 10), "a_l") #not graphical sequence
     g2.draw()
 
 
 
-
-
-    # #zad 5 k_regular graph
-    # g3 = generate_k_regular_graph(6, 3)
-    # g3.draw()
+    #zad 5 k_regular graph
+    g3 = generate_k_regular_graph(6, 3)
+    g3.draw()
